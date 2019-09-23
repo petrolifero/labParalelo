1483e2b1c82e64ecd082d8e0b17c061e7f9198f9
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2019-09-23 11:37:25 -0300

    fix OMP_NUM_THREADS name

--- performance.sh
+++ performance.sh
@@ -7,7 +7,9 @@ ulimit -s unlimited
 echo Com MPI 
 time mpirun -np 20 build/mainMPI
 echo COm MPI e OpenMP
+set OMP_NUM_THREADS=4
 time mpirun -np 20 build/mainOpenMP_MPI
 echo Com openMP 
+set OMP_NUM_THREADS=4
 time build/mainOpenMP
 
